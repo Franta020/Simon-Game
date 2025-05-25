a212355c593ed71d109add869ed095f1cc3c7171
Franta020 <oofranta@gmail.com>
2025-05-25 20:41:47 +0200

    game complete

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ var buttonCollors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var randomChosenColor = "";
 var userclickePattern = [];
+var gameLevel = 0;
 
 //create random number betwee 0 - 4 //
 function nextSequence() {
@@ -10,22 +11,25 @@ function nextSequence() {
   gamePattern.push(randomChosenColor);
   animatePress(randomChosenColor);
   playSound(randomChosenColor);
+  gameLevel += 1;
+  $("h1").text("level " + gameLevel);
+  $(".btn").on("click", function () {
+    handler(this);
+  });
 }
-
-$(".btn").click(function () {
-  handler(this);
-});
 
 function handler(button) {
   var userChosenColor = $(button).attr("id");
   userclickePattern.push(userChosenColor);
-  console.log(userclickePattern);
-  playSound(userChosenColor);
-  animatePress(userChosenColor);
+  checkAnswer();
 }
 
 $("body").keypress(function () {
-  nextSequence();
+  if (gameLevel === 0) {
+    nextSequence();
+  } else {
+    return;
+  }
 });
 
 function playSound(name) {
@@ -35,7 +39,35 @@ function playSound(name) {
 
 function animatePress(name) {
   $("." + name)
-    .animate({ opacity: "0" }, "0.01")
-    .delay("0.01")
-    .animate({ opacity: "1" }, "0.01");
+    .fadeOut(100)
+    .fadeIn(100);
+}
+
+function checkAnswer() {
+  var lastColor = userclickePattern.length - 1;
+  if (userclickePattern[lastColor] !== gamePattern[lastColor]) {
+    gameOver();
+    $(".btn").off("click");
+  } else {
+    playSound(userclickePattern[lastColor]);
+    animatePress(userclickePattern[lastColor]);
+    if (userclickePattern.length === gamePattern.length) {
+      $(".btn").off("click");
+      userclickePattern = [];
+      setTimeout(nextSequence, 1000);
+    }
+  }
+}
+
+function gameOver() {
+  var error = new Audio("./sounds/wrong.mp3");
+  error.play();
+  $("h1").text("Game over, pres any key to restart");
+  gameLevel = 0;
+  gamePattern = [];
+  userclickePattern = [];
+  $("body").addClass("game-over");
+  setTimeout(() => {
+    $("body").removeClass("game-over");
+  }, 300);
 }
